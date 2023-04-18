cb04b040aba83c95d85fa91de05e57d5cae79fe7
venu <venu@localhost.localdomain>
2023-04-18 20:49:14 +0530

    Working basic client

--- main.py
+++ main.py
@@ -1,48 +1,46 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Dict
 from bigchaindb_driver import BigchainDB
 from bigchaindb_driver.crypto import generate_keypair
+from fastapi.responses import JSONResponse
+import os
+
+class CreateTransaction(BaseModel):
+    seed: str
+    asset: Dict[str, Dict[str, str]]
+    metaData: Dict[str, str]
 
 app = FastAPI()
 
-@app.get("/getkeypair")
-def root():
-    alice= generate_keypair()
-    return alice
+bdb_root_url = 'http://localhost:9984'
+bdb = BigchainDB(bdb_root_url)
 
-@app.post("/todo")
-def create_todo():
-    bdb_root_url = 'http://localhost:9984'  # Use YOUR BigchainDB Root URL here
-    alice, bob = generate_keypair(), generate_keypair()
-    bdb = BigchainDB(bdb_root_url)
+@app.get("/get_seed")
+def get_seed():
+    seed = os.urandom(32)
+    data = {"result": "success", "data": {"seed": seed.hex()}}
+    return JSONResponse(content=data)
 
-    bicycle_asset = {
-        'data': {
-            'bees': 'knees123',
-        },
-    }
-
-    bicycle_asset_metadata = {
-        'bees': 'knees123'
-    }
+@app.post("/create_transaction")
+def create_transaction(data: CreateTransaction):
+    seed = bytes.fromhex(data.seed)
+    transaction_key_pair = generate_keypair(seed)
 
     prepared_creation_tx = bdb.transactions.prepare(
         operation='CREATE',
-        signers=alice.public_key,
-        asset=bicycle_asset,
-        metadata=bicycle_asset_metadata
+        signers=transaction_key_pair.public_key,
+        asset=data.asset,
+        metadata=data.metaData
     )
-
-    print(prepared_creation_tx)
-
     fulfilled_creation_tx = bdb.transactions.fulfill(
         prepared_creation_tx,
-        private_keys=alice.private_key
+        private_keys=transaction_key_pair.private_key
     )
-
-    print('.................................................................')
-    print(fulfilled_creation_tx)
 
     sent_creation_tx = bdb.transactions.send_commit(fulfilled_creation_tx)
     return sent_creation_tx
 
-
+@app.get("/get_transaction/{id}")
+def get_transaction(id: str):
+    return bdb.transactions.get(asset_id= id)
